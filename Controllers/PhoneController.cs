f2e875eadd9ae5b5ec71c20ea6b3742877754fd3
hemetaf-test <fawtbt@gmail.com>
2024-04-22 07:34:27 +0330

    Added all folders to the staging area

---
+++ Controllers/PhoneController.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+using MvcPhone.Data;
+
+namespace MvcPhone.Controllers
+{
+    public class PhoneController : Controller
+    {
+        private readonly MvcPhoneContext _context;
+
+        public PhoneController(MvcPhoneContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Phone
+        
+        public async Task<IActionResult> Index(string searchString)
+        {
+                 if (_context.Phone == null)
+                {
+                    return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+                }
+
+                var phones = from m in _context.Phone
+                            select m;
+
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    phones = phones.Where(s => s.Name!.Contains(searchString));
+                }
+
+                return View(await phones.ToListAsync());
+            
+        }
+
+        // GET: Phone/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var phone = await _context.Phone
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (phone == null)
+            {
+                return NotFound();
+            }
+
+            return View(phone);
+        }
+
+        // GET: Phone/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Phone/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Color,Price")] Phone phone)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(phone);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(phone);
+        }
+
+        // GET: Phone/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var phone = await _context.Phone.FindAsync(id);
+            if (phone == null)
+            {
+                return NotFound();
+            }
+            return View(phone);
+        }
+
+        // POST: Phone/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Color,Price")] Phone phone)
+        {
+            if (id != phone.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(phone);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PhoneExists(phone.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(phone);
+        }
+
+        // GET: Phone/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var phone = await _context.Phone
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (phone == null)
+            {
+                return NotFound();
+            }
+
+            return View(phone);
+        }
+
+        // POST: Phone/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var phone = await _context.Phone.FindAsync(id);
+            if (phone != null)
+            {
+                _context.Phone.Remove(phone);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PhoneExists(int id)
+        {
+            return _context.Phone.Any(e => e.Id == id);
+        }
+    }
+}
